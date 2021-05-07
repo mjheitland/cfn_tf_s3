bf37d2182f5cb7290706466d9e05a5fa4d045ef3
Michael Heitland <heitlm@amazon.com>
2021-05-07 19:03:29 +0200

    updating versions.tf file

--- tf_with_cfn/versions.tf
+++ tf_with_cfn/versions.tf
@@ -1,13 +1,8 @@
-#--- 0_tfstate/config.tf ---
-
 terraform {
   required_version = "~> 0.15"
   required_providers {
     aws = ">= 3.0.0"
   }
-#  backend "s3" {
-#    key = "0_tfstate.tfstate"
-#  }
 }
 
 provider "aws" {
