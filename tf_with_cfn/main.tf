9b4806bc6e4e55b228c98df695acddcbc7226381
Michael Heitland <heitlm@amazon.com>
2021-05-10 15:51:43 +0200

    using CFN template from external file

--- tf_with_cfn/main.tf
+++ tf_with_cfn/main.tf
@@ -12,58 +12,56 @@ variable "bucket_name" {
 data "template_file" "s3_cfn" {
   template = file("${path.module}/s3_cfn.yaml")
   vars = {
-    bucket_name = var.bucket_name
+    BucketNameParameter = var.bucket_name
   }
 }
-
-#resource "aws_cloudformation_stack" "tf_with_cfn_s3" {
-#  name = "tf_with_cfn_s3"
-#  template_body = data.template_file.s3_cfn.rendered
-#}
-
-
 
 resource "aws_cloudformation_stack" "tf_with_cfn_s3" {
   name = "tf-with-cfn-s3"
-
-  parameters = {
-    BucketNameParameter = var.bucket_name
-  }
-
-  template_body = <<STACK
-Parameters:
-  BucketNameParameter:
-    AllowedPattern: ^[0-9a-zA-Z]+([0-9a-zA-Z-.]*[0-9a-zA-Z])*$
-    ConstraintDescription: Bucket name can include numbers, lowercase letters, uppercase letters, periods (.), and hyphens (-). It cannot start or end with a hyphen (-).
-    Type: String
-    Description: Globally unique bucket name using characters and dashes.
-Resources:
-  S3Bucket:
-    Type: AWS::S3::Bucket
-    Properties:
-      BucketName: !Ref BucketNameParameter
-      VersioningConfiguration:
-        Status: Enabled
-      AccessControl: Private
-      # DeleteionPolicy: Retain
-      PublicAccessBlockConfiguration:
-        BlockPublicAcls: True
-        BlockPublicPolicy: True
-        IgnorePublicAcls: True
-        RestrictPublicBuckets: True
-      BucketEncryption: 
-        ServerSideEncryptionConfiguration: 
-        - ServerSideEncryptionByDefault:
-            SSEAlgorithm: AES256        
-Outputs:
-  BucketArn:
-    Value: 
-      !GetAtt
-        - S3Bucket
-        - Arn
-    Description: Arn of the Amazon S3 bucket.
-STACK
+  template_body = data.template_file.s3_cfn.rendered
 }
+
+# resource "aws_cloudformation_stack" "tf_with_cfn_s3" {
+#   name = "tf-with-cfn-s3"
+# 
+#   parameters = {
+#     BucketNameParameter = var.bucket_name
+#   }
+# 
+#   template_body = <<STACK
+# Parameters:
+#   BucketNameParameter:
+#     AllowedPattern: ^[0-9a-zA-Z]+([0-9a-zA-Z-.]*[0-9a-zA-Z])*$
+#     ConstraintDescription: Bucket name can include numbers, lowercase letters, uppercase letters, periods (.), and hyphens (-). It cannot start or end with a hyphen (-).
+#     Type: String
+#     Description: Globally unique bucket name using characters and dashes.
+# Resources:
+#   S3Bucket:
+#     Type: AWS::S3::Bucket
+#     Properties:
+#       BucketName: !Ref BucketNameParameter
+#       VersioningConfiguration:
+#         Status: Enabled
+#       AccessControl: Private
+#       # DeleteionPolicy: Retain
+#       PublicAccessBlockConfiguration:
+#         BlockPublicAcls: True
+#         BlockPublicPolicy: True
+#         IgnorePublicAcls: True
+#         RestrictPublicBuckets: True
+#       BucketEncryption: 
+#         ServerSideEncryptionConfiguration: 
+#         - ServerSideEncryptionByDefault:
+#             SSEAlgorithm: AES256        
+# Outputs:
+#   BucketArn:
+#     Value: 
+#       !GetAtt
+#         - S3Bucket
+#         - Arn
+#     Description: Arn of the Amazon S3 bucket.
+# STACK
+# }
 
 output "bucket_arn" {
   value = aws_cloudformation_stack.tf_with_cfn_s3.outputs.BucketArn
