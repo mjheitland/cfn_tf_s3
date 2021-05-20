b0c26f6a2400049eecbcf4cb90113a6504ca341e
Michael Heitland <heitlm@amazon.com>
2021-05-20 18:53:33 +0200

    adding a comment

--- tf_with_cfn/main.tf
+++ tf_with_cfn/main.tf
@@ -30,6 +30,7 @@ resource "aws_cloudformation_stack" "tf_with_cfn_s3" {
 }
 
 # Alternative: we can also store the CFN code inline
+#
 # resource "aws_cloudformation_stack" "tf_with_cfn_s3" {
 #   name = "tf-with-cfn-s3"
 # 
